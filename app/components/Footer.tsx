f5a39713662e3e23409dc643d4790aac77b92562
Dzakwan Fadhlullah <dzakwan22001@mail.unpad.ac.id>
2026-03-13 20:34:29 +0700

    feat: add address to footer for better accessibility

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="pt-4 pb-8">
+    <footer className="pt-4 pb-8 flex flex-col items-center gap-2">
+      <p className="text-center text-[10px] text-gray-400 tracking-wider">
+        Jl. R. E. Martadinata · Palembang
+      </p>
       <p className="text-center text-xs text-gray-500 tracking-wide font-medium">
         © 2026 Joffi Ramen · Hak Cipta Dilindungi
       </p>
